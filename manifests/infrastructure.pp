4dd7758112607471a48b0bf758b5c92dc9f056e9
root <root@datanetx.comm>
2016-04-13 17:01:21 -0400

    added new roles

--- manifests/infrastructure.pp
+++ manifests/infrastructure.pp
@@ -1,7 +1,7 @@
 class roles::infrastructure {
-#   include profiles::dn-hosts
    include profiles::dn-sudoers
    include profiles::dn-resolv
+   include profiles::dn-hosts
    include profiles::dn-users
    include profiles::dn-ntp
 }
